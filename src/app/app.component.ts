ee955875d4edcc60ba5e0f1e6b94527f3d8be148
Sergey Shehovcov <s.shehovcov@gk-adept.ru>
2024-12-09 23:15:42 +0300

    feat: first render

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angular10course';
+  public title = 'angular10course';
+  private money = 3000;
+
+  public getSalary = () => {
+    return this.money * 1.2;
+  }
 }
